d0f6577cda38c00fd4b870a57064d87c355111b1
yukino <yukino161043261@gmail.com>
2024-12-04 20:13:40 +0800

    Otaku

--- snippet/ch15.test.js
+++ snippet/ch15.test.js
@@ -9,12 +9,14 @@ test("Test_set", () => {
     // set() 拦截设置属性操作
     set(target, key, val, recv) {
       console.log("set");
+      // Object.defineProperty(recv, key, { value: val });
       Reflect.set(target, key, val, recv /* po */);
-    },
-    // defineProperty() 拦截 Object.defineProperty()
+      return true;
+    }, // defineProperty() 拦截 Object.defineProperty()
     defineProperty(target, key, val) {
       console.log("defineProperty");
       Reflect.defineProperty(target, key, val);
+      return true;
     },
   };
   let op = new Proxy(target, handler);
@@ -22,9 +24,6 @@ test("Test_set", () => {
   // op.foo = 2 ->
   // handler.set(target, key, val, recv), 输出 set ->
   // Reflect.set(target, key, val, recv) ->
-  // Object.defineProperty(recv, val) ->
+  // Object.defineProperty(recv, key, { value: val }) ->
   // handler.defineProperty(target, key, val), 输出 defineProperty
-
-  // set
-  // defineProperty
 });
