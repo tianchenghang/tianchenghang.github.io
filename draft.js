31b3d39533127fcb7a60330e8d4a3370302a29b6
Tiancheng <yukino161043261@gmail.com>
2024-12-07 21:58:43 +0800

    Otaku

--- draft.js
+++ draft.js
@@ -1,23 +1,12 @@
-const fs = require("fs");
-const thunkify = require("./snippet/thunkify");
-let readFileThunk = thunkify(fs.readFile);
-function* genFunc() {
-  let data1 /* Buffer */ = yield readFileThunk("./package.json");
-  console.log(data1.toString());
-  let data2 /* Buffer */ = yield readFileThunk("./README.md");
-  console.log(data2.toString());
-}
-function go(genFunc) {
-  let gen = genFunc();
-
-  function callback(err, data) {
-    let result /* { value: Thunk, done: boolean } */ = gen.next(data);
-    if (result.done) {
-      return;
-    }
-    result.value(callback);
-  }
-  callback();
+function timeout(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
 }
 
-go(genFunc);
+async function log(value, ms) {
+  await timeout(ms);
+  console.log(value);
+}
+
+log("hello world", 3000);
