a5e87ba7eeed03e3b066fab40533e54474c6301d
Tiancheng <yukino161043261@gmail.com>
2024-12-08 15:48:06 +0800

    Otaku

---
+++ snippet/x.js
@@ -0,0 +1,3 @@
+console.log("x1");
+await new Promise((resolve) => setTimeout(resolve, 1000));
+console.log("x2");
---
+++ snippet/z.js
@@ -0,0 +1,3 @@
+import "./x.js";
+import "./y.js";
+console.log("z"); // x1 y x2 z
